f6d1c2316da898f40ef6b3830be528407aab89eb
tobiasfremming <tobias.fremming@gmail.com>
2025-07-09 16:36:00 +0200

    refactor: update window dimensions and title for improved display

--- src/utilities/window.hpp
+++ src/utilities/window.hpp
@@ -7,9 +7,9 @@
 #include <string>
 
 // Constants
-const int         windowWidth     = 1366;
-const int         windowHeight    = 768;
-const std::string windowTitle     = "Glowbox";
+const int         windowWidth     = 1366*1.8;
+const int         windowHeight    = 768*1.8;
+const std::string windowTitle     = "Shader";
 const GLint       windowResizable = GL_FALSE;
 const int         windowSamples   = 4;
 
